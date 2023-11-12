8ef2330c83ca3612e8e86408a5d6b60081198eda
Sora <91284120+sorapanel@users.noreply.github.com>
2023-11-13 02:21:38 +0900

    Update views.py
    コメント追加

--- line_bot_ai/views.py
+++ line_bot_ai/views.py
@@ -6,13 +6,18 @@ from django.views.decorators.csrf import csrf_exempt
 from utils import message_creater
 from line_bot_ai.line_message import LineMessage
 
+#csrf無効
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
+        #デコード
         request = json.loads(request.body.decode('utf-8'))
         data = request['events'][0]
+        #メッセージ受信
         message = data['message']
         reply_token = data['replyToken']
+        #送信用テキスト作成後LINEmessageAPIへリクエスト
         line_message = LineMessage(message_creater.create_single_text_message(message['text']))
+        #返信
         line_message.reply(reply_token)
         return HttpResponse("ok")
