0234d126da66b3e8b788b6af9dd35bfa23c82868
Sora <91284120+sorapanel@users.noreply.github.com>
2023-11-13 02:17:39 +0900

    Update message_creater.py
    コメント追加

--- utils/message_creater.py
+++ utils/message_creater.py
@@ -1,6 +1,10 @@
+#送信メッセージ作成
 def create_single_text_message(message):
     if message is not None:
+        #文字数カウント
         message = str(len(message)) + '文字です(空白を含む)\n' + str(len(message.replace('　', '').replace('\t', '').replace('\n', ''))) + '文字です(空白を含まない)'
+
+    #送信用形式
     test_message = [
                 {
                     'type': 'text',
